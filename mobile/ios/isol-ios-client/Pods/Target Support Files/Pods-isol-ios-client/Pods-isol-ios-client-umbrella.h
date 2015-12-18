933823e4a78a01d66d5e5ac94e012513fc2b4dc8
Steve <steve@tgiltd.com>
2015-12-18 07:11:21 -0800

    Added base iOS mobile project. Service can get iso file strings as an array.

---
+++ mobile/ios/isol-ios-client/Pods/Target Support Files/Pods-isol-ios-client/Pods-isol-ios-client-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_isol_ios_clientVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_isol_ios_clientVersionString[];
+
---
+++ mobile/ios/isol-ios-client/isol-ios-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  isol-ios-client
+//
+//  Created by Stephen Smith on 12/18/15.
+//  Copyright © 2015 Stephen Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
